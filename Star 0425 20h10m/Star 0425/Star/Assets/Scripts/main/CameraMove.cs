98e12c58bbf78e570abda5dd1fe5945e53cda16c
yuji <a>
2019-05-01 19:05:12 +0900

    わーい

--- Star 0425 20h10m/Star 0425/Star/Assets/Scripts/main/CameraMove.cs
+++ Star 0425 20h10m/Star 0425/Star/Assets/Scripts/main/CameraMove.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    public GameGenerator gameGenerator;
     private Vector3 oldPos;
     private GameObject player;
     private float target;
     private int saveCount;
+    private int count;
 
     void Start()
     {
         saveCount = 0;
+        count = 0;
         oldPos = Vector3.zero;
         player = GameObject.FindWithTag("Player");
     }
@@ -21,11 +24,17 @@ public class CameraMove : MonoBehaviour
         transform.position += new Vector3(v3.x / 4.0f, v3.y / 4.0f);
         oldPos = player.transform.position;
         transform.LookAt(player.transform.position);
-        if (GameGenerator.Star / 20 != saveCount)
+        int star = gameGenerator.GetComponent<GameGenerator>().star;
+        if (star / 25 != saveCount && count == 60)
         {
-            target = -5 - GameGenerator.Star / 5 * 0.4f - transform.position.z;
-            saveCount = GameGenerator.Star / 20;
+            target = (-5 - (star / 5 * 0.4f)) - transform.position.z;
+            saveCount = star / 25;
+            count = 0;
         }
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + target / 60f);
+        if (count < 60)
+        {
+            //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + target / 60f);
+            count++;
+        }
     }
 }
