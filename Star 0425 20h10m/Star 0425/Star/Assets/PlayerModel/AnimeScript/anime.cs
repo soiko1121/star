1a3a77159bbaa8ce0c7ba0fb2ec3f12fe7aee452
konata016 <k>
2019-05-07 14:13:14 +0900

    ステージ修正&モデル、モーション追加
    ステージ：
    Assets→MyNewStage→Prefab→Stage
    モデル、モーション
    Assets→PlayerModel
    
    の中にあります

---
+++ Star 0425 20h10m/Star 0425/Star/Assets/PlayerModel/AnimeScript/anime.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anime : MonoBehaviour
+{
+    private Animator Anime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anime = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D)) Anime.SetBool("IsRight", true);
+        else Anime.SetBool("IsRight", false);
+
+        if (Input.GetKey(KeyCode.A)) Anime.SetBool("IsLeft", true);
+        else Anime.SetBool("IsLeft", false);
+
+        if (Input.GetKey(KeyCode.Space)) Anime.SetBool("IsDamage", true);
+        else Anime.SetBool("IsDamage", false);
+
+        if (!Input.anyKey) Anime.SetBool("IsFloat", true);
+        else Anime.SetBool("IsFloat", false);
+    }
+}
