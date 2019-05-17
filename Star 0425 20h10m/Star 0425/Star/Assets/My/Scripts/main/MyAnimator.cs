7e6f19bd446b44a9a17e9779feb2f9ce7ed61071
Unknown <konata016@gmail.com>
2019-05-18 04:39:17 +0900

    修正版

--- Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/main/MyAnimator.cs
+++ Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/main/MyAnimator.cs
@@ -6,12 +6,12 @@ public class MyAnimator : MonoBehaviour
 {
     public Animator anim;
 
-    public float LeftMax = -1f;         //左の傾け角度の最大値
-    public float LeftNormal = -0.5f;    
+    public float LeftMax = -1f;         //左の3段階目の傾き実行値
+    public float LeftNormal = -0.5f;    //左の2段階目の傾き実行値
     public float LeftMin = 0f;          //左のモーションを実行する値
 
-    public float RightMax = 1f;         //右の傾け角度の最大値
-    public float RightNormal = 0.5f;
+    public float RightMax = 1f;         //右の3段階目の傾き実行値
+    public float RightNormal = 0.5f;    //右の2段階目の傾き実行値
     public float RightMin = 0f;         //右のモーションを実行する値
 
     public static float X
@@ -73,10 +73,10 @@ public class MyAnimator : MonoBehaviour
             #endregion
 
             #region 左向き角度段階
-            if (x < LeftMin && x > LeftNormal) anim.SetBool("IsLeftFloat1", true);
+            if (x < LeftMin && x >= LeftNormal) anim.SetBool("IsLeftFloat1", true);
             else anim.SetBool("IsLeftFloat1", false);
 
-            if (x < LeftNormal && x > LeftMax) anim.SetBool("IsLeftFloat2", true);
+            if (x < LeftNormal && x >= LeftMax) anim.SetBool("IsLeftFloat2", true);
             else anim.SetBool("IsLeftFloat2", false);
 
             if (x < LeftMax) anim.SetBool("IsLeftFloat3", true);
@@ -95,10 +95,10 @@ public class MyAnimator : MonoBehaviour
             #endregion
 
             #region 右向き角度段階
-            if (x > RightMin && x < RightNormal) anim.SetBool("IsRightFloat1", true);
+            if (x > RightMin && x <= RightNormal) anim.SetBool("IsRightFloat1", true);
             else anim.SetBool("IsRightFloat1", false);
 
-            if (x > RightNormal && x < RightMax) anim.SetBool("IsRightFloat2", true);
+            if (x > RightNormal && x <= RightMax) anim.SetBool("IsRightFloat2", true);
             else anim.SetBool("IsRightFloat2", false);
 
             if (x > RightMax) anim.SetBool("IsRightFloat3", true);
