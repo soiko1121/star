34e17bbc41b2c14d02b9338a39508ea10719c2e6
konata016 <k>
2019-05-18 20:59:10 +0900

    ステージ調整

--- Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/main/MyAnimator.cs
+++ Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/main/MyAnimator.cs
@@ -41,8 +41,6 @@ public class MyAnimator : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log(X);
-
         if (X < LeftMin || X > RightMin) Animation(X);
         else
         {
