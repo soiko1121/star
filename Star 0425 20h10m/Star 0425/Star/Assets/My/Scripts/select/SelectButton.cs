4878915604d0c5bc2aa68dc8032cdf7861f7ff7f
Horiuchi Fubuki <fubuki199914@gmail.com>
2019-05-19 15:51:29 +0900

    セレクト画面作り始めました

---
+++ Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/select/SelectButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectButton : MonoBehaviour
+{
+    public void LeftOnClick()
+    {
+        SelectStage.StageSelect--;
+        if (SelectStage.minStageNumber > SelectStage.StageSelect)
+        {
+            SelectStage.StageSelect = SelectStage.maxStageNumber;
+        }
+    }
+    public void RightOnClick()
+    {
+        SelectStage.StageSelect++;
+        if (SelectStage.maxStageNumber < SelectStage.StageSelect)
+        {
+            SelectStage.StageSelect = SelectStage.minStageNumber;
+        }
+    }
+    public void StartOnClick()
+    {
+        SceneManager.LoadScene("LoadScene");
+    }
+}
