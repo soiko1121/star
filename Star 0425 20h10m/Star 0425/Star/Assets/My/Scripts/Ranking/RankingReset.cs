2a951497aee32773679237604a08304755b6f3cc
Horiuchi Fubuki <fubuki199914@gmail.com>
2019-05-19 21:51:15 +0900

    ランキングリセットつくったー

---
+++ Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/Ranking/RankingReset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankingReset : MonoBehaviour
+{
+    public void ResetOnClick()
+    {
+        for (int loopStage = 0; loopStage < SelectStage.maxStageNumber; loopStage++)
+        {
+            for (int loopRank = 1; loopRank < Ranking.maxRank; loopRank++)
+            {
+                PlayerPrefs.SetFloat("STAGE" + loopStage + "RANK" + loopRank, 0);
+            }
+        }
+        PlayerPrefs.Save();
+    }
+}
