80de00ab8b214a15a05b9f5478dbd8a085136c39
Horiuchi Fubuki <fubuki199914@gmail.com>
2019-05-21 14:50:37 +0900

    ランキングシーン組み込み＋ああ～＾ボタンの音ぉ～

---
+++ Star 0425 20h10m/Star 0425/Star/Assets/My/Music/Script/SEControl_Button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEControl_Button : MonoBehaviour
+{
+    private AudioSource audioController;
+    void Start()
+    {
+        audioController = gameObject.GetComponent<AudioSource>();
+    }
+    public void ButtonSe()
+    {
+        audioController.PlayOneShot(audioController.clip);
+    }
+}
