12fb73c6bfb6f22e967089fd3d6221d4e1641bc6
yuji <a>
2019-05-23 22:19:03 +0900

    まってくれぇ～

--- Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/main/PlayerMove.cs
+++ Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/main/PlayerMove.cs
@@ -6,11 +6,13 @@ public class PlayerMove : MonoBehaviour
 {
     private Vector3 gyro, gyroSet, moveVec, velocitySet;
     private Rigidbody playerRB;
-    public float speed, slowdown, set2DSpeed;
+    public float slowdown, set2DSpeed;
     public DebugText debugText;
     public TimeGenerator timeGenerator;
     public float ySpeed2D;
-    public float gyroLimit;
+    public float defY;
+    public Vector2 speedV2;
+    public Vector2 gyroLimit;
     public Vector2 limit;
     public Vector2 rotaLimit;
     public MyAnimator myAnimator;
@@ -21,7 +23,6 @@ public class PlayerMove : MonoBehaviour
     //private Vector2 acceleration;
     private Vector2 pm;
     private Vector3 target;
-    private Vector2 speedV2;
     private Vector3 oldpos;
     public List<Vector3> PosList
     {
@@ -48,7 +49,6 @@ public class PlayerMove : MonoBehaviour
         target = Vector3.zero;
         gyro = Vector3.zero;
         gyroSet = Vector3.zero;
-        speedV2 = new Vector2(speed, speed);
         oldpos = Vector3.zero;
 
         accelerationCount = Vector2.zero;
@@ -85,48 +85,51 @@ public class PlayerMove : MonoBehaviour
 
         if (!DebugPC.pc)
         {
-            Vector3 v3 = transform.position;
-            //重力感知
-            gyro.x = Mathf.Clamp(Input.gyro.gravity.x * 2.0f, -gyroLimit, gyroLimit);
-            gyro.y = Mathf.Clamp(((Input.gyro.gravity.z + 0.7f) * 2.0f), -gyroLimit, gyroLimit);
-            debugText.GetComponent<DebugText>().text = Input.gyro.gravity.x.ToString() + ":::::::" + Input.gyro.gravity.y.ToString() + ":::::::" + Input.gyro.gravity.z.ToString();
-            if (gyro.x > 0)
-            {
-                pm.x = 1;
-            }
-            else
-            {
-                pm.x = -1;
-            }
-            if (gyro.y > 0)
-            {
-                pm.y = 1;
-            }
-            else
-            {
-                pm.y = -1;
-            }
-            pm *= PM;
-            if (gyro.y < 0)
-                gyro.y *= 1.6f;
-            accelerationCount.x = (Mathf.Abs(gyro.x) / (int)(gyroLimit / maxAcceleration.x));
-            accelerationCount.y = (Mathf.Abs(gyro.y) / (int)(gyroLimit / maxAcceleration.y));
-            if (viewSet == View.back)
-            {
-                if (Mathf.Abs(transform.position.x + accelerationCount.x * 0.5f * (gyroLimit / maxAcceleration.x) * speedV2.x * pm.x) < limit.x)
-                    v3.x = transform.position.x + accelerationCount.x * 0.5f * (gyroLimit / maxAcceleration.x) * speedV2.x * pm.x;
-                else
-                    v3.x = transform.position.x;
-                if (Mathf.Abs(transform.position.y + accelerationCount.y * 0.5f * (gyroLimit / maxAcceleration.y) * speedV2.y * pm.y) < limit.y)
-                    v3.y = transform.position.y + accelerationCount.y * 0.5f * (gyroLimit / maxAcceleration.y) * speedV2.y * pm.y;
-                else
-                    v3.y = transform.position.y;
-                v3.z = 0;
-                //if(accelerationCount.y * 0.5f * (gyroLimit / maxAcceleration.y) * speedV2.y * pm.y + 0.4f > 0.7f)
-                //    transform.rotation = new Quaternion(-0.7f, 0, 0, transform.rotation.w);
-                //else
-                //transform.rotation = new Quaternion(-(accelerationCount.y * 0.5f * (gyroLimit / maxAcceleration.y) * speedV2.y * pm.y) + 0.4f, 0, 0, transform.rotation.w);
-            }
+            Vector3 v3 = GetGyro();
+            ////重力感知
+            //gyro.x = Mathf.Clamp(Input.gyro.gravity.x * 2.0f, -gyroLimit, gyroLimit);
+            //gyro.y = Mathf.Clamp(((Input.gyro.gravity.z + 0.7f) * 2.0f), -gyroLimit, gyroLimit);
+
+            //float a = (20f * Input.gyro.gravity.y * Input.gyro.gravity.y + Input.gyro.gravity.y) / 21f;
+            //float b = (20f * Input.gyro.gravity.x * Input.gyro.gravity.x + Input.gyro.gravity.x) / 21f;
+            //debugText.GetComponent<DebugText>().text = Input.gyro.gravity.x.ToString() + "\n" + a.ToString() + "\n" + b.ToString();
+            //if (gyro.x > 0)
+            //{
+            //    pm.x = 1;
+            //}
+            //else
+            //{
+            //    pm.x = -1;
+            //}
+            //if (gyro.y > 0)
+            //{
+            //    pm.y = 1;
+            //}
+            //else
+            //{
+            //    pm.y = -1;
+            //}
+            //pm *= PM;
+            //if (gyro.y < 0)
+            //    gyro.y *= 1.6f;
+            //accelerationCount.x = (Mathf.Abs(gyro.x) / (int)(gyroLimit / maxAcceleration.x));
+            //accelerationCount.y = (Mathf.Abs(gyro.y) / (int)(gyroLimit / maxAcceleration.y));
+            //if (viewSet == View.back)
+            //{
+            //    if (Mathf.Abs(transform.position.x + accelerationCount.x * 0.5f * (gyroLimit / maxAcceleration.x) * speedV2.x * pm.x) < limit.x)
+            //        v3.x = transform.position.x + accelerationCount.x * 0.5f * (gyroLimit / maxAcceleration.x) * speedV2.x * pm.x;
+            //    else
+            //        v3.x = transform.position.x;
+            //    if (Mathf.Abs(transform.position.y + accelerationCount.y * 0.5f * (gyroLimit / maxAcceleration.y) * speedV2.y * pm.y) < limit.y)
+            //        v3.y = transform.position.y + accelerationCount.y * 0.5f * (gyroLimit / maxAcceleration.y) * speedV2.y * pm.y;
+            //    else
+            //        v3.y = transform.position.y;
+            //    v3.z = 0;
+            //    //if(accelerationCount.y * 0.5f * (gyroLimit / maxAcceleration.y) * speedV2.y * pm.y + 0.4f > 0.7f)
+            //    //    transform.rotation = new Quaternion(-0.7f, 0, 0, transform.rotation.w);
+            //    //else
+            //    //transform.rotation = new Quaternion(-(accelerationCount.y * 0.5f * (gyroLimit / maxAcceleration.y) * speedV2.y * pm.y) + 0.4f, 0, 0, transform.rotation.w);
+            //}
             //else
             //{
             //    v3 = new Vector3(
@@ -158,21 +161,13 @@ public class PlayerMove : MonoBehaviour
             }
             Vector3 pos = Vector3.zero;
             if (Input.GetKey(KeyCode.A))
-            {
                 pos.x += -1;
-            }
             if (Input.GetKey(KeyCode.D))
-            {
                 pos.x += 1;
-            }
             if (Input.GetKey(KeyCode.W))
-            {
                 pos.y += 1;
-            }
             if (Input.GetKey(KeyCode.S))
-            {
                 pos.y += -1;
-            }
             //pos = target - transform.position;
             //pos = pos.normalized * 10f;
             //playerRB.AddForce(pos);
@@ -184,6 +179,52 @@ public class PlayerMove : MonoBehaviour
         {
             PosList.RemoveAt(0);
         }
+    }
+    Vector3 GetGyro()
+    {
+        Vector3 v3 = transform.position;
+        Vector3 gyro = Vector3.zero;
+        Vector3 inputGyro = Input.gyro.gravity;
+        //重力感知
+        gyro.x = (20f * inputGyro.x * inputGyro.x + inputGyro.x) / 21f;
+        gyro.y = (20f * inputGyro.y * inputGyro.y + inputGyro.y) / 21f;
+
+        if (gyro.x > gyroLimit.x)
+            gyro.x = gyroLimit.x;
+        //if (gyro.y > gyroLimit.y + defY)
+        //    gyro.y = gyroLimit.y + defY;
+        //if (inputGyro.y < 0)
+        //    gyro.y = 0;
+        //debugText.GetComponent<DebugText>().text = gyro.x.ToString() + "\n" + gyro.y.ToString();
+
+        if (inputGyro.x > 0)
+            pm.x = 1;
+        else
+            pm.x = -1;
+        if (gyro.y > defY)
+            pm.y = 1;
+        else
+            pm.y = -1;
+
+        pm *= PM;
+
+        accelerationCount.x = (int)(Mathf.Abs(gyro.x) / (gyroLimit.x / maxAcceleration.x));
+        if (gyro.y > defY)
+            accelerationCount.y = (int)(Mathf.Abs(gyro.y) / (gyroLimit.y - defY / maxAcceleration.y));
+        else
+            accelerationCount.y = (int)(Mathf.Abs(gyro.y) / (defY / maxAcceleration.y));
+        debugText.GetComponent<DebugText>().text = accelerationCount.x.ToString() + "\n" + accelerationCount.y.ToString();
+
+        if (Mathf.Abs(transform.position.x + accelerationCount.x * (speedV2.y / maxAcceleration.x) * pm.x) < limit.x)
+            v3.x = transform.position.x + accelerationCount.x * (speedV2.y / maxAcceleration.x) * pm.x;
+        else
+            v3.x = transform.position.x;
+        if (Mathf.Abs(transform.position.y + accelerationCount.y * (speedV2.y / maxAcceleration.y) * pm.y) < limit.y)
+            v3.y = transform.position.y + accelerationCount.y * (speedV2.y / maxAcceleration.y) * pm.y;
+        else
+            v3.y = transform.position.y;
+        v3.z = 0;
+        return v3;
     }
     Vector3 GetMousePosition()
     {
