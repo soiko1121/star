c7c6104a5825c4b1014e5a9c7e9a10b577bffa9c
yuji <a>
2019-05-24 12:52:46 +0900

    超調整

--- Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/select/SelectStage.cs
+++ Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/select/SelectStage.cs
@@ -39,6 +39,7 @@ public class SelectStage : MonoBehaviour
             buttons[i].enabled = false;
         }
         buttons[StageSelectNumber-1].enabled = true;
+        PlanetGenerator.stageNumber = StageSelectNumber - 1;
         ExpText();
         GetComponent<Text>().text = kari;
         if(sb.GetComponent<SelectButton>().flag)
