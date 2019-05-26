9243cd07b5611e7529a06ee891dd4e0011b0e351
Horiuchi Fubuki <fubuki199914@gmail.com>
2019-05-26 21:03:34 +0900

    リザルト＜完＞
    Scripts>result>ResultRank>ShowRankでランク付けできるので各自ステージ班よろしく

---
+++ Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/title/First.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class First : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectStage.StageSelectNumber = SelectStage.minStageNumber;   
+    }
+}
