0de425a322bc782640f305a062f7794f5362678d
Horiuchi Fubuki <fubuki199914@gmail.com>
2019-05-27 19:45:44 +0900

    BGM 増えないよ

--- Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/select/ReturnButtonCon.cs
+++ Star 0425 20h10m/Star 0425/Star/Assets/My/Scripts/select/ReturnButtonCon.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class ReturnButtonCon : MonoBehaviour
 {
+    private GameObject 
+        titleBGM,
+        button;
+    private void Start()
+    {
+        titleBGM = GameObject.Find("BGM");
+        button = GameObject.Find("ButtonSE");
+    }
     public void retrunT()
     {
+        titleBGM.GetComponent<ButtonDontDestroy>().DestroyThisObject();
+        button.GetComponent<ButtonDontDestroy>().DestroyThisObject();
         Invoke("loadT", 0.5f);
     }
     private void loadT()
